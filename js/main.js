4e8bbf599023827150718f5adc6728bb50a62f27
Lucas Dec <dec.lukasz@gmail.com>
2020-01-11 08:40:15 +0100

    add pause option

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const btnStart = document.querySelector(".start");
 let pauseActive = false;
 let startTime, pauseTime;
+let countLoop = 0;
 
 const btnLoop = document.querySelector(".loop");
 const btnPause = document.querySelector(".pause");
@@ -8,9 +9,18 @@ const btnStop = document.querySelector(".stop");
 const btnReset = document.querySelector(".reset");
 
 const displayTime = document.querySelector(".timer");
+const loopList = document.querySelector("ul.loop");
 let indexInterval;
 let time = 0;
-let min, sec, mill;
+let min, sec, mil;
+
+function pause() {
+  pauseActive = !pauseActive;
+  clearInterval(indexInterval);
+  pauseTime = time;
+  btnPause.classList.add("hidden");
+  btnStart.classList.remove("hidden");
+}
 
 const timer = () => {
   const currentTime = new Date().getTime();
@@ -27,12 +37,20 @@ const timer = () => {
 
 const start = () => {
   btnStart.classList.add("hidden");
-  startTime = new Date().getTime();
-  indexInterval = setInterval(timer, 10);
   btnLoop.classList.remove("hidden");
   btnPause.classList.remove("hidden");
   btnStop.classList.remove("hidden");
   btnReset.classList.add("hidden");
+  if (!pauseActive) {
+    startTime = new Date().getTime();
+    indexInterval = setInterval(timer, 10);
+  }
+  if (pauseActive) {
+    pauseActive = !pauseActive;
+    startTime = new Date().getTime() - time;
+    indexInterval = setInterval(timer, 10);
+  }
 };
 
 btnStart.addEventListener("click", start);
+btnPause.addEventListener("click", pause);
